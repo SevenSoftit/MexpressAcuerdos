bc90789dc5e17ea664a2dac306547214bd4fbc72
Gustavo-ZC <gustavo.zuniga@sevensoftit.com>
2019-07-12 18:03:50 -0600

    Varios acuerdos
    Varios acuerdos

--- Sevensoft.Mexpress.Backend.Common/Ac_Mtr_Agreement_Detail.cs
+++ Sevensoft.Mexpress.Backend.Common/Ac_Mtr_Agreement_Detail.cs
@@ -11,35 +11,28 @@ namespace Sevensoft.Mexpress.Backend.Common
             Pk_Ac_Trade_Agreement = 0;
             Pk_Cat_Currency = 0;
             Pk_Glb_Products = 0;
+            Product_Id_Alias = "";
+            Product_Name = "";
+            Id_Currency = "";
+            Product_Amount = 0;            
             Creation_Date = Convert.ToDateTime("1900-01-01");
             Creation_User = "";
             Modification_Date = Convert.ToDateTime("1900-01-01");
-            Modification_User = "";            
-            Product_Id_Alias = "";
-            Product_Name = "";
-            Product_Amount = 0;
-            Id_Currency = "";
+            Modification_User = "";                     
             Active = false;
-            All_Products = false;     
-            Name_Currency = "";
-            
-
         }
         public Int32 Pk_Cat_Agreement_Details { get; set; }
         public Int32 Pk_Ac_Trade_Agreement { get; set; }
         public Int32 Pk_Cat_Currency { get; set; }
         public Int32 Pk_Glb_Products { get; set; }
-        public DateTime Creation_Date { get; set; }
+         public String Product_Id_Alias { get; set; }
+        public String Product_Name { get; set; }
+        public String Id_Currency { get; set; }
+        public Decimal Product_Amount { get; set; }
+         public DateTime Creation_Date { get; set; }
         public String Creation_User { get; set; }
         public DateTime Modification_Date { get; set; }
         public String Modification_User { get; set; }
-        public String Product_Id_Alias { get; set; }
-        public String Product_Name { get; set; }
-        public Decimal Product_Amount { get; set; }
-        public String Id_Currency { get; set; }
-        public Boolean Active { get; set; }
-        public Boolean All_Products { get; set; }
-        public String Name_Currency { get; set; }
-        
+        public Boolean Active { get; set; }       
     }
 }
