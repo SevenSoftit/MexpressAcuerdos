db0ac8c2bdb9a7ce4cde5addf8e5fa5254be9e93
Gustavo-ZC <gustavo.zuniga@sevensoftit.com>
2019-08-30 18:15:17 -0600

    Varios reporte
    Varios reporte

--- Sevensoft.Mexpress.Backend.Web.Api/Controllers/PdfController.cs
+++ Sevensoft.Mexpress.Backend.Web.Api/Controllers/PdfController.cs
@@ -2,17 +2,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System.IO;
 using System;
-using iText.Html2pdf;
-using iText.Kernel.Pdf;
-using iText.Layout.Font;
-using iText.Html2pdf.Resolver.Font;
-using iText.StyledXmlParser.Css.Media;
-using System.Text;
-using iText.Layout;
-using iText.Kernel.Geom;
-using iText.Layout.Element;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
 namespace Sevensoft.Mexpress.Backend.Web.Api.Controllers
+
 {
     [Route("api/[controller]")]
     [ApiController]
@@ -34,40 +28,145 @@ namespace Sevensoft.Mexpress.Backend.Web.Api.Controllers
         {
             try
             {
-                string output = string.Empty;                
+                string output = string.Empty;
                 output = string.Format("{0}", configuration.GetValue<string>("Files:RutaDestinoReporteAcuerdos"));
                 FileStream fs = new FileStream(output, FileMode.Create);
-                //MemoryStream ms = new MemoryStream();
-                
-                PdfDocument pdf = new PdfDocument(new PdfWriter(fs));   
-                //PdfDocument pdf = new PdfDocument(new PdfWriter(ms));    
-                Document document = new Document(pdf, PageSize.LETTER);
-  
-                document.SetMargins(0f, 0f, 0f, 0f);
-                
-                // document.Add(new Paragraph("Hola PDF \n Trabajando con archivos PDF"));
-                Table table = new Table(7);
-                
+                Document document = new Document(iTextSharp.text.PageSize.A2, 30f, 20f, 50f, 40f);
+                PdfWriter pw = PdfWriter.GetInstance(document, fs);
+                pw.PageEvent = new HeaderFooter();
+                document.Open();
 
+                BaseFont bf = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1250, BaseFont.EMBEDDED);
+                Font fontTextE = new Font(bf, 14, 1, BaseColor.BLACK);
+                Font fontTextT = new Font(bf, 12, 0, BaseColor.BLACK);
+                PdfPTable table = new PdfPTable(11);
+                table.WidthPercentage = 100f;
+
+                string[] oddArray = new string[] { "Código", "Nombre artículo", "Tienda de venta", "Nombre vendedor", "Cliente", "Tipo de factura", "Nº Factura", "Serie", "Cantidad", "Costo", "Fecha de venta" };
+                foreach (string val in oddArray)
+                {
+                    PdfPCell cell = new PdfPCell();
+                    cell = new PdfPCell(new Paragraph(val, fontTextE));
+                    cell.HorizontalAlignment = Element.ALIGN_CENTER;
+                    table.AddCell(cell);
+                }
+                foreach (var item in list.AgreementProductInfoDetailList)
+
+                {
+                    PdfPCell cell = new PdfPCell();
+
+                    cell = new PdfPCell(new Paragraph(item.Product_Id, fontTextT));
+                    cell.HorizontalAlignment = Element.ALIGN_CENTER;
+                    table.AddCell(cell);
+
+                    cell = new PdfPCell(new Paragraph(item.Product_Name, fontTextT));
+                    cell.HorizontalAlignment = Element.ALIGN_CENTER;
+                    table.AddCell(cell);
+
+                    cell = new PdfPCell(new Paragraph(item.Name_Store, fontTextT));
+                    cell.HorizontalAlignment = Element.ALIGN_CENTER;
+                    table.AddCell(cell);
+
+                    cell = new PdfPCell(new Paragraph(item.Name_Vendor, fontTextT));
+                    cell.HorizontalAlignment = Element.ALIGN_CENTER;
+                    table.AddCell(cell);
+
+                    cell = new PdfPCell(new Paragraph(item.Client_Name, fontTextT));
+                    cell.HorizontalAlignment = Element.ALIGN_CENTER;
+                    table.AddCell(cell);
+
+                    cell = new PdfPCell(new Paragraph(item.Name_Document, fontTextT));
+                    cell.HorizontalAlignment = Element.ALIGN_CENTER;
+                    table.AddCell(cell);
+
+                    cell = new PdfPCell(new Paragraph(item.Bill_Id.ToString(), fontTextT));
+                    cell.HorizontalAlignment = Element.ALIGN_CENTER;
+                    table.AddCell(cell);
+
+                    cell = new PdfPCell(new Paragraph(item.Product_Serie, fontTextT));
+                    cell.HorizontalAlignment = Element.ALIGN_CENTER;
+                    table.AddCell(cell);
+
+                    cell = new PdfPCell(new Paragraph(item.Product_Quantity.ToString(), fontTextT));
+                    cell.HorizontalAlignment = Element.ALIGN_CENTER;
+                    table.AddCell(cell);
+
+
+                    cell = new PdfPCell(new Paragraph(item.Product_Cost.ToString(), fontTextT));
+                    cell.HorizontalAlignment = Element.ALIGN_CENTER;
+                    table.AddCell(cell);
+
+                    cell = new PdfPCell(new Paragraph(item.Date_Invoice.ToString(), fontTextT));
+                    cell.HorizontalAlignment = Element.ALIGN_CENTER;
+                    table.AddCell(cell);
+
+                    //table.AddCell(new Paragraph(item.Product_Id));
+                    // table.AddCell(new Paragraph(item2.Product_Name));
+                    // table.AddCell(new Paragraph(item2.Name_Store));
+                    // table.AddCell(new Paragraph(item2.Name_Vendor));
+                    // table.AddCell(new Paragraph(item2.Client_Name));
+                    // table.AddCell(new Paragraph(item2.Name_Document));
+                    // table.AddCell(new Paragraph(item2.Bill_Id.ToString()));
+                    // table.AddCell(new Paragraph(item2.Product_Serie));
+                    // table.AddCell(new Paragraph(item2.Product_Quantity.ToString()));
+                    // table.AddCell(new Paragraph(item2.Product_Cost.ToString()));
+                    // table.AddCell(new Paragraph(item2.Date_Invoice.ToString()));
+                }
+
+                document.Add(table);
                 document.Close();
 
-                // byte[] bytesStream = ms.ToArray();
-                // ms = new MemoryStream();
-                // ms.Write(bytesStream,0,bytesStream.Length);
-                // ms.Position = 0; 
-
-                // return new FileStreamResult(ms, "application/pdf");
-                return Ok("Documento creado");            
-                }
+                return Ok("Documento creado");
+            }
             catch (Exception ex)
             {
                 return BadRequest(ex);
             }
 
+            // Aca puede ir
         }
-
-
         #endregion Region [Methods]
+    }
+
+    class HeaderFooter : PdfPageEventHelper
+    {
+        public override void OnEndPage(PdfWriter writer, Document document)
+        {
+            // Configuracion Header
+            PdfPTable tbHeader = new PdfPTable(3);
+            tbHeader.TotalWidth = document.PageSize.Width - document.LeftMargin - document.RightMargin;
+            tbHeader.DefaultCell.Border = 0;
+
+            tbHeader.AddCell(new Paragraph());
+
+            PdfPCell _cell = new PdfPCell(new Paragraph("Lista de productos"));
+            _cell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _cell.Border = 0;
+
+            tbHeader.AddCell(_cell);
+            tbHeader.AddCell(new Paragraph());
+
+            tbHeader.WriteSelectedRows(0, -1, document.LeftMargin, writer.PageSize.GetTop(document.TopMargin) + 40, writer.DirectContent);
+
+            // Configuracion Footer
+            PdfPTable tbFooter = new PdfPTable(3);
+            tbFooter.TotalWidth = document.PageSize.Width - document.LeftMargin - document.RightMargin;
+            tbFooter.DefaultCell.Border = 0;
+
+            tbFooter.AddCell(new Paragraph());
+
+            _cell = new PdfPCell(new Paragraph("Acuerdos Mexpress"));
+            _cell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _cell.Border = 0;
+
+            tbFooter.AddCell(_cell);
+            _cell = new PdfPCell(new Paragraph("Página " + writer.PageNumber));
+            _cell.HorizontalAlignment = Element.ALIGN_RIGHT;
+            _cell.Border = 0;
+            tbFooter.AddCell(_cell);
+
+            tbFooter.WriteSelectedRows(0, -1, document.LeftMargin, writer.PageSize.GetBottom(document.BottomMargin) - 5, writer.DirectContent);
+        }
     }
 
 }
